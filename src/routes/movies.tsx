6d49831d712380644c099b13e8247a697f785910
nkululek0 <nkululekovuyo0103@gmail.com>
2025-11-03 13:30:18 +0200

    (Update): movies -> adds content for fetching movie data

--- src/routes/movies.tsx
+++ src/routes/movies.tsx
@@ -1,19 +1,60 @@
 import { Search } from '@/components/search';
 
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, useLoaderData } from '@tanstack/react-router';
+
+import type { MediaItem } from '@/api/types';
+import { getData } from '@/api/api';
+
+import { Media } from '@/components/media';
 
 export const Route = createFileRoute('/movies')({
   component: RouteComponent,
+  loader: async () => {
+    const data = await getData();
+
+    if (data && data.length > 0) {
+      return data;
+    }
+    else {
+      throw Error();
+    }
+  },
+  pendingComponent: () => {
+    return <div className='loading'>Loading...</div>;
+  },
+  errorComponent: () => {
+    return <div className='error'>There was an issue loading the component data</div>;
+  }
 })
 
 function RouteComponent() {
+  const data = useLoaderData({ from: '/movies' });
+  const movies = data?.filter((item: MediaItem) => item.category === 'Movie');
 
   return (
     <section className='page-wrapper'>
       <Search placeHolderText='Search for movies' />
       <section className='movies-wrapper'>
         <h2>Movies</h2>
-        <section className="movies"></section>
+        <section className="movies">
+          {
+            movies?.map((item, index) => {
+              return (
+                <Media
+                  key={ index }
+                  type='secondary'
+                  title={ item.title }
+                  year={ item.year }
+                  category={ item.category }
+                  rating={ item.rating }
+                  isBookmarked={ item.isBookmarked }
+                  isTrending={ item.isTrending }
+                  thumbnail={ item.thumbnail }
+                />
+              );
+            })
+          }
+        </section>
       </section>
     </section>
   );
