16eceb4966aa323e213c60520be92b5abf9dbd57
nkululek0 <nkululekovuyo0103@gmail.com>
2025-11-11 09:49:39 +0200

    (Update): api -> adds styling for primary type media card content to make it more visible

--- src/components/media/Media.tsx
+++ src/components/media/Media.tsx
@@ -19,7 +19,7 @@ const icons = {
       fill='#FFF'
     />
   </svg>),
-  star: (<svg viewBox="0 0 20 20" width='10' height='10' xmlns="http://www.w3.org/2000/svg">
+  star: (<svg viewBox="0 0 20 20" fill="none" width='10' height='10' xmlns="http://www.w3.org/2000/svg">
     <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
     <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
     <g id="SVGRepo_iconCarrier">
