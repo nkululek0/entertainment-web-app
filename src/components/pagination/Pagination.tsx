332ab4d932a686b1d99e876ba3ab53ec0c9f532b
nkululek0 <nkululekovuyo0103@gmail.com>
2025-11-17 10:41:09 +0200

    (Upadate): pagination -> completes pagination component

--- src/components/pagination/Pagination.tsx
+++ src/components/pagination/Pagination.tsx
@@ -3,60 +3,59 @@ import _ from 'lodash';
 import type { PageNavigationSymbols } from './Pagination.types';
 import style from './Pagination.module.css';
 
-const paginationRange = (totalPages: number, page: number, limit: number, siblings: number) => {
-  let result: PageNavigationSymbols[] = [];
-  let totalPageInArray = siblings;
+const paginationRange = (totalPages: number, page: number, siblings: number): PageNavigationSymbols[] => {
+  if (siblings > totalPages) return _.range(1, totalPages + 1);
 
-  if (totalPageInArray > totalPages) result = _.range(1, totalPages + 1);
-
-  let leftSiblingsIndex = Math.max(page - siblings, 1);
-  let rightSiblingsIndex = Math.min(page + siblings, totalPages);
-  let showLeftDots = leftSiblingsIndex > 2;
-  let showRightDots = rightSiblingsIndex < totalPages - 2;
+  const leftSiblingsIndex = Math.max(page - siblings, 1);
+  const rightSiblingsIndex = Math.min(page + siblings, totalPages);
+  const showLeftDots = leftSiblingsIndex > 2;
+  const showRightDots = rightSiblingsIndex < totalPages - 2;
 
   if(!showLeftDots && showRightDots) {
     let leftItemsCount = 3 * siblings;
     let leftRange = _.range(1, leftItemsCount + 1);
 
-    result = [...leftRange];
+    return [...leftRange];
   }
   else if (showLeftDots && !showRightDots) {
     let rightItemsCount = 3 * siblings;
     let rightRange = _.range(totalPages - rightItemsCount + 1, totalPages + 1);
 
-    result = [1, ...rightRange];
+    return [1, ...rightRange];
   }
   else {
     let middleRange = _.range(leftSiblingsIndex, rightSiblingsIndex + 1);
 
-    result = [1, ...middleRange];
+    return [1, ...middleRange];
   }
-
-  return result;
 };
 
 export type PaginationProps = {
-  totalPages: number;
-  page: number;
-  limit: number;
-  siblings: number;
-  onPageChange: (pageSymbol: PageNavigationSymbols) => void;
+  totalPages: number
+  page: number
+  siblings: number
+  onPageChange: (pageSymbol: PageNavigationSymbols) => void
 };
 
 export function Pagination(props: PaginationProps) {
-  const { totalPages, page, limit, siblings, onPageChange } = props;
-  const pageArray = paginationRange(totalPages, page, limit, siblings);
+  const { totalPages, page, siblings, onPageChange } = props;
+  const pageArray = paginationRange(totalPages, page, siblings);
 
   return (
     <>
       <nav className={ style['pagination-container'] }>
         <ul className={ style['pagination'] }>
-          <li className={ style['page-item'] } onClick={ () => onPageChange('&laquo;') }>
-            <span className={ style['page-link'] }>&laquo;</span>
-          </li>
-          <li className={ style['page-item'] } onClick={ () => onPageChange('&lsaquo;') }>
-            <span className={ style['page-link'] }>&lsaquo;</span>
-          </li>
+          {
+            page != 1 &&
+            <>
+              <li className={ style['page-item'] } onClick={ () => onPageChange('&laquo;') }>
+                <span className={ style['page-link'] }>&laquo;</span>
+              </li>
+              <li className={ style['page-item'] } onClick={ () => onPageChange('&lsaquo;') }>
+                <span className={ style['page-link'] }>&lsaquo;</span>
+              </li>
+            </>
+          }
           {
             pageArray.map((item, index) => {
               if (item === page) {
@@ -84,12 +83,17 @@ export function Pagination(props: PaginationProps) {
               }
             })
           }
-          <li className={ style['page-item'] } onClick={ () => onPageChange('&rsaquo;') }>
-            <span className={ style['page-link'] }>&rsaquo;</span>
-          </li>
-          <li className={ style['page-item'] } onClick={ () => onPageChange('&raquo;') }>
-            <span className={ style['page-link'] }>&raquo;</span>
-          </li>
+          {
+            totalPages != page &&
+            <>
+              <li className={ style['page-item'] } onClick={ () => onPageChange('&rsaquo;') }>
+                <span className={ style['page-link'] }>&rsaquo;</span>
+              </li>
+              <li className={ style['page-item'] } onClick={ () => onPageChange('&raquo;') }>
+                <span className={ style['page-link'] }>&raquo;</span>
+              </li>
+            </>
+          }
         </ul>
       </nav>
     </>
