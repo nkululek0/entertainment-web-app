1b13e277dc5338d84a33b18e549207f733fa9aa0
nkululek0 <nkululekovuyo0103@gmail.com>
2025-11-18 12:23:34 +0200

    (Update): card-details -> adds function to declare and fetch custom urls which keeps api.ts file DRY

--- src/api/api.ts
+++ src/api/api.ts
@@ -5,6 +5,18 @@ const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 type ShowType = 'movie' | 'tv';
 
+const getUrl = (path: string, page?: number, query?: string) => {
+  const url = new URL(`${ BASE_URL }/${ path }`);
+
+  url.searchParams.set('api_key', API_KEY);
+  url.searchParams.set('language', 'en-US');
+
+  if (page) url.searchParams.set('page', page.toString());
+  if (query) url.searchParams.set('query', query);
+
+  return url;
+};
+
 const processResponse = async (url: string) => {
   const response = await fetch(url);
   const responseData = await response.json();
@@ -19,22 +31,26 @@ const processResponse = async (url: string) => {
 
 const API = {
   getTrending: async (type: ShowType) => {
-    const data = await processResponse(`${ BASE_URL }/trending/${ type }/day?api_key=${ API_KEY }&language=en-US`);
+    const url = getUrl(`trending/${ type }/day`);
+    const data = await processResponse(url.href);
 
     return data as ShowResponse;
   },
   getTopRated: async (type: ShowType, page: number = 1) => {
-    const data = await processResponse(`${ BASE_URL }/${ type }/top_rated?api_key=${ API_KEY }&language=en-US&page=${ page }`);
+    const url = getUrl(`${ type }/top_rated`, page);
+    const data = await processResponse(url.href);
 
     return data as ShowResponse;
   },
   getPopular: async (type: ShowType, page: number = 1) => {
-    const data = await processResponse(`${ BASE_URL }/${ type }/popular?api_key=${ API_KEY }&language=en-US&page=${ page }`);
+    const url = getUrl(`${ type }/popular`, page);
+    const data = await processResponse(url.href);
 
     return data as ShowResponse;
   },
   getSearch: async (type: ShowType, query: string, page: number = 1) => {
-    const data = await processResponse(`${ BASE_URL }/search/${ type }?api_key=${ API_KEY }&language=en-US&query=${ query }&page=${ page }`);
+    const url = getUrl(`${ type }/search`, page, query);
+    const data = await processResponse(url.href);
 
     return data as ShowResponse;
   },
