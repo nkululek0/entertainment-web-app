1edf1956e509ba4cf5ef31a2afee72bcf701af80
nkululek0 <nkululekovuyo0103@gmail.com>
2025-11-19 17:20:19 +0200

    (Update): show-details -> adds media content on card details component

--- src/components/card-details/CardDetails.tsx
+++ src/components/card-details/CardDetails.tsx
@@ -1,7 +1,44 @@
-export function CardDetails() {
+import style from './CardDetails.module.css';
+
+type CardDetailsProps = {
+  backdropPath: string;
+  genres: Array<{ id: number, name: string }>;
+  overview: string;
+  posterPath: string;
+  releaseDate?: string;
+  firstAirDate?: string;
+  status?: string;
+  tagline: string;
+  title?: string;
+  name?: string;
+  video?: boolean;
+  voteAverage: number;
+};
+
+export function CardDetails(props: CardDetailsProps) {
+  const { backdropPath, genres, overview, posterPath, releaseDate, firstAirDate, status, tagline, title, name, video, voteAverage } = props;
+  const cardTitle = title ?? name;
+  const imageBaseURL = 'https://image.tmdb.org/t/p/original';
+
   return (
-    <div>
-      <h1>CardDetails</h1>
-    </div>
+    <section className={ style['card-details-wrapper'] }>
+      <section className='media-wrapper'>
+        <div className='media'>
+          <div className='media-first-child'>
+            <div className={ style['poster-container'] }>
+              <img src={ imageBaseURL + posterPath } alt={ cardTitle } />
+            </div>
+            <div className={ style['backdrop-container'] }>
+              <img src={ imageBaseURL + backdropPath } alt='' />
+            </div>
+          </div>
+          <div className={ style['rating-container'] }>
+            <span>{ Number(voteAverage.toFixed(1)) * 10 }%</span>
+          </div>
+          <h2 className={ style['card-title'] }>{ cardTitle }</h2>
+        </div>
+      </section>
+      <section className='overview-wrapper'></section>
+    </section>
   );
 };
