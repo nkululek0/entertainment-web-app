b7f374d263acd632bfe44d9ef62d7817be7879e5
nkululek0 <nkululekovuyo0103@gmail.com>
2025-11-27 14:17:31 +0200

    (Create): show-cast -> adds cast details component

---
+++ src/components/cast-card/CastCard.tsx
@@ -0,0 +1,37 @@
+import style from './CastCard.module.css';
+
+const IMAGE_BASE_URL = import.meta.env.VITE_IMAGE_BASE_URL;
+
+type CastProps = {
+  gender: number
+  id: number
+  name: string
+  profile_path: string | null
+  character: string
+  order: number
+};
+
+export function CastCard(props: CastProps) {
+  const { name, profile_path, character } = props;
+
+  return (
+    <section className={ style['cast'] }>
+      <div className='image-container'>
+        {
+          !profile_path &&
+          <div className={ style['image'] }>
+            <p>No Image</p>
+          </div>
+        }
+        {
+          profile_path &&
+          <img src={ IMAGE_BASE_URL + profile_path } alt={ name } className={ style['image'] } />
+        }
+      </div>
+      <div className='cast-info'>
+        <h4>{ name }</h4>
+        <p>{ character }</p>
+      </div>
+    </section>
+  );
+};
