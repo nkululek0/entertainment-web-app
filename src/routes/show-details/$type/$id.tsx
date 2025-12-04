eeab4014fd337709ff8d6f122b19fc6539970c71
nkululek0 <nkululekovuyo0103@gmail.com>
2025-12-04 15:46:37 +0200

    (Update): show-media -> adds backdrop and posters show details media content

--- src/routes/show-details/$type/$id.tsx
+++ src/routes/show-details/$type/$id.tsx
@@ -1,5 +1,6 @@
 import { createFileRoute, useLoaderData } from '@tanstack/react-router';
 import ScrollContainer from 'react-indiana-drag-scroll';
+import { useState } from 'react';
 
 import { type ShowCast, type ShowImages } from '@/api/types';
 
@@ -8,6 +9,8 @@ import { CardDetails } from '@/components/card-details';
 import { CastCard } from '@/components/cast-card';
 
 import API from '@/api/api';
+
+const IMAGE_BASE_URL = import.meta.env.VITE_IMAGE_BASE_URL;
 
 type ShowType = 'movie' | 'tv';
 
@@ -19,6 +22,13 @@ export const Route = createFileRoute('/show-details/$type/$id')({
       await API.getShowCast(params.type as ShowType, Number(params.id)),
       await API.getShowImages(params.type as ShowType, Number(params.id))
     ]);
+
+    if (!showDetails || !showCast || !showImages) throw new Error();
+
+    showImages.backdrops.sort((a, b) => b.vote_average - a.vote_average);
+    showImages.backdrops = showImages.backdrops.slice(0, 5);
+    showImages.posters.sort((a, b) => b.vote_average - a.vote_average).slice(0, 5);
+    showImages.posters = showImages.posters.slice(0, 5);
 
     return {
       showDetails: showDetails,
@@ -42,8 +52,21 @@ export const Route = createFileRoute('/show-details/$type/$id')({
     }
 });
 
+type MediaSlideType = 'video' |'backdrop' | 'poster';
+const slideShowMediaImage = (image: ShowImages['backdrops'][0] | ShowImages['posters'][0], index: number) => {
+  return (
+    <img
+      key={ index }
+      src={ `${ IMAGE_BASE_URL }/${ image.file_path }` }
+      alt={ image.file_path }
+      className='media-slideshow-image'
+    />
+  );
+};
+
 function RouteComponent() {
   const { showDetails, showCast, showImages } = useLoaderData({ from: '/show-details/$type/$id' });
+  const [activeSlide, setActiveSlide] = useState<MediaSlideType>('backdrop');
 
   return (
     <section className='show-details-wrapper'>
@@ -66,7 +89,7 @@ function RouteComponent() {
       <div className='cast-wrapper'>
         <h3>Cast</h3>
         <div className='cast-slideshow-container'>
-          <ScrollContainer className="scroll-container cast-slideshow">
+          <ScrollContainer className='scroll-container cast-slideshow'>
             {
               showCast.cast.map((cast: ShowCast['cast'][0], index: number) => {
                 return (
@@ -85,16 +108,40 @@ function RouteComponent() {
           </ScrollContainer>
         </div>
       </div>
-      <div className="show-media-wrapper">
+      <div className='show-media-wrapper'>
         <h3>Media</h3>
+        <menu className='media-menu'>
+          <ul>
+            <li onClick={ () => setActiveSlide('video') } >
+              <span>Videos</span>
+              <div className={`menu-item-underline ${ activeSlide == 'video' && 'active' }`}></div>
+            </li>
+            <li onClick={ () => setActiveSlide('backdrop') }>
+              <span>Backdrops</span>
+              <div className={`menu-item-underline ${ activeSlide == 'backdrop' && 'active' }`}></div>
+            </li>
+            <li onClick={ () => setActiveSlide('poster') }>
+              <span>Posters</span>
+              <div className={`menu-item-underline ${ activeSlide == 'poster' && 'active' }`}></div>
+            </li>
+          </ul>
+        </menu>
         <div className='media-slideshow-container'>
-          <ScrollContainer className="scroll-container media-slideshow">
+          <ScrollContainer className='scroll-container media-slideshow'>
             {
-              showImages.backdrops.map((backdrop: ShowImages['backdrops'][0], index: number) => {
-                return (
-                  <img key={ index } src={ backdrop.file_path } alt={ backdrop.file_path } />
-                );
-              })
+              activeSlide == 'video' && ''
+            }
+            {
+              activeSlide == 'backdrop' &&
+              showImages.backdrops.map((backdrop: ShowImages['backdrops'][0], index: number) => (
+                slideShowMediaImage(backdrop, index)
+              ))
+            }
+            {
+              activeSlide == 'poster' &&
+              showImages.posters.map((poster: ShowImages['posters'][0], index: number) => (
+                slideShowMediaImage(poster, index)
+              ))
             }
           </ScrollContainer>
         </div>
