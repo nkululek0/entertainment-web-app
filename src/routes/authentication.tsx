5aeeef40f62429580d3e268f3e2f2f0e838f76e8
nkululek0 <nkululekovuyo0103@gmail.com>
2025-12-15 16:39:32 +0200

    (Update): user-profile -> adds styling to authentication route

--- src/routes/authentication.tsx
+++ src/routes/authentication.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import { createFileRoute } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/authentication')({
@@ -5,20 +7,43 @@ export const Route = createFileRoute('/authentication')({
 });
 
 function RouteComponent() {
+  const [authenticationType, setAuthenticationType] = useState<'login' | 'signup'>('login');
+
   return (
+    <>
     <div className='authentication-container'>
-      <section className='authentication-wrapper'>
-        <h4>Login</h4>
-        <form className='form login-form'>
-          <input type='email' className='form-input'/>
-          <input type='password' className='form-input'/>
-          <button>Login to your account</button>
-          <div className='signup-container'>
+      {
+        authenticationType == 'login' &&
+        <section className='authentication-wrapper'>
+          <h2>Login</h2>
+          <form className='authentication-form login-form'>
+            <input type='email' className='form-input' placeholder='Email address'/>
+            <input type='password' className='form-input' placeholder='Password'/>
+            <button>Login to your account</button>
+          </form>
+          <div className='authentication-actions-container'>
             <p>Don't have an account?</p>
-            <button>Sign Up</button>
+            <button onClick={ () => setAuthenticationType('signup') }>Sign Up</button>
           </div>
-        </form>
-      </section>
+        </section>
+      }
+      {
+        authenticationType == 'signup' &&
+        <section className='authentication-wrapper'>
+          <h2>Sign Up</h2>
+          <form className='authentication-form signup-form'>
+            <input type='email' className='form-input' placeholder='Email address'/>
+            <input type='password' className='form-input' placeholder='Password'/>
+            <input type='password' className='form-input' placeholder='Repeat password'/>
+            <button>Create an account</button>
+          </form>
+          <div className='authentication-actions-container'>
+            <p>Already have an account?</p>
+            <button onClick={ () => setAuthenticationType('login') }>Login</button>
+          </div>
+        </section>
+      }
     </div>
+    </>
   );
 };
