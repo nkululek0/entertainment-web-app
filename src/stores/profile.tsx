ff7ede68ff14ffed1a4e39fe1d6b0e482d9e6351
nkululek0 <nkululekovuyo0103@gmail.com>
2026-01-28 12:34:41 +0200

    (Update): renamed profile.ts to profile.tsx as context provider now deals with useEffect and returns tsx

---
+++ src/stores/profile.tsx
@@ -0,0 +1,69 @@
+import { createContext, useContext, useState, useEffect } from 'react';
+
+import { toast } from 'react-toastify';
+
+import { getSession } from '@/lib/supabase-client.ts';
+import { LoadSpinner } from '@/components/load-spinner';
+
+
+type Profile = {
+  username: string
+}
+type Context = {
+  profile: Profile
+  setIsLoggedIn: React.Dispatch<React.SetStateAction< boolean >>
+  isLoggedIn: boolean
+};
+
+const ProfileContext = createContext<Context | undefined>(undefined);
+
+export const ProfileProvider = ({ children }: { children: React.ReactNode }) => {
+  const [profile, setProfile] = useState<Profile>({ username: '' });
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    const retrieveSession = async () => {
+      const { error, data } = await getSession();
+      setIsLoading(false);
+
+      if (error) toast.error(`Error while getting session: ${ error.message }`)
+
+      if (data.session) {
+        const user = data.session.user;
+
+        setProfile({ username: user?.email as string });
+        setIsLoggedIn(true);
+      }
+      else {
+        setProfile({ username: '' });
+        setIsLoggedIn(false);
+      }
+    };
+
+    retrieveSession();
+  }, [isLoggedIn]);
+
+  return (
+    <ProfileContext.Provider value={{ profile, setIsLoggedIn, isLoggedIn }}>
+      { isLoading &&
+        <div style={{ margin: '0% 50%' }}>
+          <LoadSpinner width={ 60 } height={ 60 } />
+        </div>
+      }
+      { !isLoading && children }
+    </ProfileContext.Provider>
+  );
+};
+
+export const useProfile = () => {
+  const context = useContext(ProfileContext);
+
+  if (!context) {
+    throw new Error('useProfile must be used within a ProfileProvider');
+  }
+
+  return context;
+};
