b7505d965556a9b2fd1b24f1d03986bbb14265d2
nkululek0 <nkululekovuyo0103@gmail.com>
2026-02-06 12:07:17 +0200

    (Update): adds database ability to update user profile details

--- src/lib/supabase-client.ts
+++ src/lib/supabase-client.ts
@@ -7,6 +7,10 @@ const supabase = createClient(supabaseUrl, supabaseKey);
 
 import { type Profile, ProfileSchema } from '@/api/types';
 
+const databaseNames = {
+  user_profiles: 'user_profiles'
+};
+
 export const signUp = async (email: string, password: string) => {
   const { error } = await supabase.auth.signUp({
     email: email,
@@ -41,10 +45,88 @@ export const logout = async () => {
   return { error };
 };
 
+export const setUserProfile = async (username: string) => {
+  const { error } = await supabase
+  .from(databaseNames.user_profiles)
+  .insert({ username: username })
+  .single();
+
+  return { error };
+};
+
+export const getUserProfile = async (username: string) => {
+  const { data, error } = await supabase
+  .from(databaseNames.user_profiles)
+  .select('*')
+  .eq('username', username)
+  .single();
+
+  return { data, error };
+};
+
+type UserProfile = {
+  [key: string]: string
+  first_name: string
+  last_name: string
+};
+
+type UserCredentials = {
+  [key: string]: string
+  password: string
+  email: string
+};
+
+type ProfileUpdateType = 'General' | 'Credentials';
+
+export const updateUserProfile = async (type: ProfileUpdateType, username: string, options: Partial<UserProfile> | Partial<UserCredentials>) => {
+  const promises = [];
+
+  if (type === 'General') {
+    for (const key in options) {
+      if (options[key]) {
+        promises.push(setUserProfileGeneralDetails(username, key, options[key]));
+      }
+    }
+  }
+  else if (type === 'Credentials') {
+    for (const key in options) {
+      if (options[key]) {
+        promises.push(setUserProfileCredentials(key, options[key]));
+      }
+    }
+  }
+
+  const response = await Promise.all(promises.map(p => p()));
+  const errors = response.filter((r) => r.error).map((r) => r.error?.message);
+
+  return { errors, data: promises.length > 0 ? { message: 'User profile updated successfully' } : null };
+};
+
 export const getSession = async () => {
   const { data, error } = await supabase.auth.getSession();
 
   return { data, error };
+};
+
+const setUserProfileGeneralDetails = (username: string, key: string, value: string) => {
+  return async () => {
+    const { error } = await supabase
+    .from(databaseNames.user_profiles)
+    .update({ [key]: value })
+    .match({ username: username });
+
+    return { error };
+  };
+};
+
+const setUserProfileCredentials = (key: string, value: string) => {
+  return async () => {
+    const { error } = await supabase.auth.updateUser({
+      [key]: value
+    });
+
+    return { error };
+  };
 };
 
 const getURL = () => {
